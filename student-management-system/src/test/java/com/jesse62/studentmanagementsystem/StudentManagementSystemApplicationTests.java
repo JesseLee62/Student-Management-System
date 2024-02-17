00728d8f065a24badffb12f76145986a67adcaad
Jesse <jesse05306@gmail.com>
2024-02-16 19:34:22 -0600

    upload student-management-system

---
+++ student-management-system/src/test/java/com/jesse62/studentmanagementsystem/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesse62.studentmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
